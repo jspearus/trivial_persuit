00ac0ebc7bbfdd624a684c2d86acb2efcc5affc5
jspearus <jspearus@gmail.com>
2023-01-23 14:32:34 -0600

    update

--- game/views.py
+++ game/views.py
@@ -132,12 +132,25 @@ def result(request):
     })
 
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 def dash(request):
     playerlist = []
     players = Player.objects.all()
     gameData = GameData.objects.filter(name='game').first()
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
+    IPAddr = get_ip()
     return render(request, 'dash.html', {
         'players': players,
         'num_players': gameData.num_players,
