958656ad34ec532906ea531cd29b3a6b3e89ea6f
jspearus <jspearus@gmail.com>
2023-03-11 03:10:23 +0000

    new branch

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Trivial Pursuit</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
