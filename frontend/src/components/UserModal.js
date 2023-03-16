42c6eed77c7c681431096989dae4679f2bfe5973
jspearus <jspearus@gmail.com>
2023-03-16 00:33:06 +0000

    update

--- frontend/src/components/UserModal.js
+++ frontend/src/components/UserModal.js
@@ -11,9 +11,15 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import PersonIcon from '@mui/icons-material/Person';
 import AddIcon from '@mui/icons-material/Add';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import { Grid } from '@material-ui/core';
+import { Grid, TextField } from '@material-ui/core';
+import { DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
 import { blue } from '@mui/material/colors';
 
 const emails = ['Player1', 'Player2'];
@@ -71,8 +77,17 @@ SimpleDialog.propTypes = {
 };
 
 export default function UserModal() {
+    const [theme, setTheme] = React.useState('Blue');
+    const [difficulty, setDifficulty] = React.useState('Medium');
     const [open, setOpen] = React.useState(false);
     const [selectedValue, setSelectedValue] = React.useState('None Selected');
+
+    const handleThemeChange = (event) => {
+        setTheme(event.target.value);
+    };
+    const handleChange = (event) => {
+        setDifficulty(event.target.value);
+    };
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -95,25 +110,85 @@ export default function UserModal() {
             <Card
                 sx={{
                     width: 300,
+                    height: 400,
                     backgroundColor: 'white',
                     // backgroundColor: '#424242',
                 }}>
                 <div>
-                    <Typography variant="h4" component="div">
-                        Curent User
-                    </Typography>
-                    <Typography variant="subtitle1" component="div">
-                        Selected: {selectedValue}
-                    </Typography>
-                    <br />
-                    <Button variant="outlined" onClick={handleClickOpen}>
-                        Select Or Create User
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle>Subscribe</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Please Enter Username (Can be whatever you want) and select difficulty
+                            </DialogContentText>
+                            <Box
+                                component="joinForm"
+                                sx={{
+                                    '& > :not(style)': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <TextField
+                                    id="outlined-controlled"
+                                    label="Enter Username"
+                                // value=""
+                                // onChange={(event) => {
+                                //     // setName(event.target.value);
+                                //     console.log(event)
+                                // }}
+                                />
+                                <FormControl >
+                                    <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={difficulty}
+                                        label="Difficulty"
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value={10}>Easy</MenuItem>
+                                        <MenuItem value={20}>Medium</MenuItem>
+                                        <MenuItem value={30}>Hard</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <FormControl >
+                                    <InputLabel id="demo-simple-select-label">Theme</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        value={theme}
+                                        label="Theme"
+                                        onChange={handleThemeChange}
+                                    >
+                                        <MenuItem value={10}>Red</MenuItem>
+                                        <MenuItem value={20}>Green</MenuItem>
+                                        <MenuItem value={30}>Blue</MenuItem>
+                                        <MenuItem value={40}>Orange</MenuItem>
+                                        <MenuItem value={50}>Purple</MenuItem>
+                                        <MenuItem value={60}>Pink</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button onClick={handleClose}>Join</Button>
+                            </DialogActions>
+                        </DialogContent>
+                    </Dialog>
+                    <Typography
+                        variant="h4"
+                        sx={{
+                            mt: 10,
+                        }}
+                    >Join Game</Typography>
+                    <Button sx={{
+                        mt: 10,
+                    }}
+                        variant="contained"
+                        onClick={handleClickOpen}>
+                        Join Game
                     </Button>
-                    <SimpleDialog
-                        selectedValue={selectedValue}
-                        open={open}
-                        onClose={handleClose}
-                    />
                 </div>
             </Card>
         </Grid >
